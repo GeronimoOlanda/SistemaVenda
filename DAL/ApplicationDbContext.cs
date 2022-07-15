c0cbc8890ebcb98f0527aabf7cec0247404fc106
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-07-14 22:00:02 -0300

    Criando o dbContext da nossa app

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVendas.Entidades;
+
+namespace SistemaVendas.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Venda> Venda { get; set; }
+        public DbSet<VendaProdutos> VendaProdutos { get; set; }
+
+        //especificando o tipo da nossa entidade
+
+        //override vamos sobrescrever o nosso metodo
+        protected override void OnModelCreating(ModelBuilder builder)
+        {   
+            //estamos dizendo que vamos sobreescrever
+            base.OnModelCreating(builder);
+
+            //dizendo que o codigoVenda e CodigoProduto são PrimaryKey
+            builder.Entity<VendaProdutos>().HasKey(x => new { x.CodigoVenda, x.CodigoProduto });
+            builder.Entity<VendaProdutos>()
+                .HasOne(x => x.Venda)   //hasOne diz que possui um alguma coisa, no nosso caso, uma venda
+                .WithMany(y => y.Produtos) //withMany diz que em uma venda, poderemos ter muitos produtos vendidos
+                .HasForeignKey(x => x.CodigoVenda);
+
+            builder.Entity<VendaProdutos>()
+             .HasOne(x => x.Produto)   //hasOne diz que possui um alguma coisa, no nosso caso, uma venda
+             .WithMany(x => x.Vendas) //withMany diz que em uma venda, poderemos ter muitos produtos vendidos
+             .HasForeignKey(x => x.CodigoProduto);
+        }
+    }
+}
