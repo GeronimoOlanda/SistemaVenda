d1f2c0486be06f085dda9bf7b65d8114cb84ac10
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-07-18 23:17:24 -0300

    Criação da categoria controller

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.DAL;
+
+namespace SistemaVendas.Controllers
+{
+    public class CategoriaController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
